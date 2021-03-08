edc37bd09df14022fa3ffefc6a8327b53749ae0f
teejaymoo <teajay.m88@gmail.com>
2021-03-08 09:29:29 -0600

    Added curl-scripts and created containers in html

---
+++ assets/scripts/auth-user/api.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const config = require('../config')
+const store = require('../store')
+
+const signUp = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data
+  })
+}
+
+const signIn = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data
+  })
+}
+
+const signOut = function () {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token // IMPORTANT
+    }
+  })
+}
+
+const changePassword = function (data) {
+  console.log('data is ', data)
+  return $.ajax({
+    url: config.apiUrl + '/change-password',
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token // IMPORTANT
+    },
+    data
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  signOut,
+  changePassword
+}
---
+++ curl-scripts/auth/sign-in.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl "https://aqueous-thicket-45658.herokuapp.com/sign-in" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
